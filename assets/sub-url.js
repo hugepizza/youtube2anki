1090ee6986420109b0da94c4967aeeab31afcdc9
hugepizza <wangleilei950325@gmail.com>
2024-01-08 15:47:28 +0800

    auto play

--- assets/sub-url.js
+++ assets/sub-url.js
@@ -16,11 +16,11 @@ window.addEventListener(
     if (e.data.action === "adjustProgress") {
       console.log(e.data.action, e.data.data)
       if (!isNaN(e.data.data)) {
-        // console.log(''e.data.action, e.data.data)
         const videoElement = document.querySelector(".video-stream")
         if (videoElement) {
           console.log("adjust")
           videoElement.currentTime = e.data.data
+          videoElement.play()
         }
       }
     }
